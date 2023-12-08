dac7a3322f04c71969771286b296d1667a765b5f
KENZY004 <150925589+KENZY004@users.noreply.github.com>
2023-12-08 16:24:01 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      kenzn
+#
+# Created:     22-09-2023
+# Copyright:   (c) kenzn 2023
+# Licence:     <your licence>
+#----------------------------------------------------------------------------
+items="python"
+index=0
+for item in items:
+    print(index,item)
+    index+=1
